85ae66912d4a1b08d146c98e7b767927569318ea
Jin0331 <jinoo@wmbio.co>
2022-01-26 09:37:49 +0900

    update

--- src/.ipynb_checkpoints/r-function-checkpoint.R
+++ src/.ipynb_checkpoints/r-function-checkpoint.R
@@ -1,19 +1,20 @@
 # install.package
-requiredPackages <- c('magrittr','survival','ranger', 'NbClust', 'survminer', 'tibble', 'stringr', 'ggplot2', 'dplyr')
+requiredPackages <- c('survival','ranger', 'NbClust', 'survminer', 'tidyverse', 'BiocManager')
 for(p in requiredPackages){
-  if(!require(p,character.only = TRUE)){install.packages(p)}}
-    
+    if(!require(p,character.only = TRUE)){install.packages(p)}}
+
+for(p in c("TCGAbiolinks", "SummarizedExperiment")){
+    if(!require(p,character.only = TRUE)){BiocManager::install(p)}}
+
 # library load
 suppressPackageStartupMessages({
-    library(magrittr)
     library(survival)
     library(ranger)
     library(NbClust)
     library(survminer)
-    library(tibble)
-    library(stringr)
-    library(ggplot2)
-    library(dplyr)
+    library(tidyverse)
+    library(TCGAbiolinks)
+    library(SummarizedExperiment)
 })
 
 # KM - plot
@@ -49,4 +50,73 @@ nb_cluster_test <- function(df){
   
   nc <- NbClust(df,min.nc=2,max.nc=9,method="kmeans", index = c("silhouette","cindex"))
   nc$All.index %>% as_tibble() %>% select("Silhouette") %>% return()
+}
+
+run_deg <- function(project, sample){
+  # Query platform Illumina HiSeq with a list of barcode 
+  query <- GDCquery(project = paste0("TCGA-", pr_name), 
+                    data.category = "Gene expression",
+                    data.type = "Gene expression quantification",
+                    experimental.strategy = "RNA-Seq",
+                    platform = "Illumina HiSeq",
+                    file.type = "results",
+                    barcode = sample %>% pull(1), 
+                    legacy = TRUE)
+  
+  # Download a list of barcodes with platform IlluminaHiSeq_RNASeqV2
+  GDCdownload(query)
+  
+  # Prepare expression matrix with geneID in the rows and samples (barcode) in the columns
+  # rsem.genes.results as values
+  LIHCRnaseqSE <- GDCprepare(query)
+  
+  # For gene expression if you need to see a boxplot correlation and AAIC plot to define outliers you can run
+  LIHCRnaseq_CorOutliers <- TCGAanalyze_Preprocessing(LIHCRnaseqSE)
+  
+  # subgroup names
+  sub0 <- sample %>% filter(group == 0) %>% pull(1)
+  sub1 <- sample %>% filter(group == 1) %>% pull(1)
+  
+  sample_subgroup <- LIHCRnaseq_CorOutliers %>% colnames() %>% lapply(X = ., FUN = function(value){
+    
+    value_trans <- str_extract_all(value, pattern = "TCGA-[:alnum:]+-[:alnum:]+-[:digit:]+") %>%  unlist()
+    subgroup_df <- tibble(sample_barcode = NA, subgroup = NA, .rows = 0)
+    
+    
+    if(value_trans %in% sub0){
+      subgroup_df <- tibble(sample_barcode = value, subgroup = 0)
+    } else {
+      subgroup_df <- tibble(sample_barcode = value, subgroup = 1)
+    }
+    
+    return(subgroup_df)
+    
+  }) %>% bind_rows()
+  
+  sub0_name <- sample_subgroup %>% filter(subgroup == 0) %>% pull(sample_barcode)
+  sub1_name <- sample_subgroup %>% filter(subgroup == 1) %>% pull(sample_barcode)
+  
+  # normalization of genes
+  dataNorm <- TCGAanalyze_Normalization(tabDF = LIHCRnaseq_CorOutliers, geneInfo =  geneInfo)
+  
+  # quantile filter of genes
+  dataFilt <- TCGAanalyze_Filtering(tabDF = dataNorm,
+                                    method = "quantile", 
+                                    qnt.cut =  0.25)
+  
+  #Diff.expr.analysis (DEA)
+  dataDEGs <- TCGAanalyze_DEA(mat1 = dataFilt[,sub0_name],
+                              mat2 = dataFilt[,sub1_name],
+                              Cond1type = "Sub-0",
+                              Cond2type = "Sub-1",
+                              fdr.cut = 0.01 ,
+                              logFC.cut = 1,
+                              method = "glmLRT",
+                              paired = F)
+  
+  # DEGs table with expression values in normal and tumor samples
+  dataDEGsFiltLevel <- TCGAanalyze_LevelTab(dataDEGs,"Sub-0","Sub-1",
+                                            dataFilt[,sub0_name],dataFilt[,sub1_name])
+  
+  return(dataDEGsFiltLevel)
 }

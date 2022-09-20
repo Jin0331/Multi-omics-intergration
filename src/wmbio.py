e7c5df01f5acbf81c1029fabdf5448a6785d0d82
Jin0331 <jinoo@wmbio.co>
2022-09-20 16:23:49 +0900

    PDB 3D structure ID adding

--- src/wmbio.py
+++ src/wmbio.py
@@ -19,7 +19,16 @@ if len(gpus) == 0:
     R_HOME = os.path.expanduser('~') + '/anaconda3/envs/multiomics-cpu/lib/R'
 else :
     R_HOME = os.path.expanduser('~') + '/anaconda3/envs/multiomics-gpu/lib/R'
-        
+
+import time
+import json
+import zlib
+from pypdb import *
+from xml.etree import ElementTree
+from urllib.parse import urlparse, parse_qs, urlencode
+import requests
+from requests.adapters import HTTPAdapter, Retry
+from collections import defaultdict    
 import re
 import pickle
 import datetime
@@ -986,6 +995,171 @@ def tmb_t_test(group, raw_path):
   t_test_result = tmb_calc(group, raw_path)
 
   return t_test_result[0]
+
+# Uniprot, PDB ID Search
+POLLING_INTERVAL = 3
+API_URL = "https://rest.uniprot.org"
+tsv_loader = "?compressed=true&fields=accession%2Creviewed%2Cid%2Cprotein_name%2Corganism_name&format=tsv"
+
+retries = Retry(total=5, backoff_factor=0.25, status_forcelist=[500, 502, 503, 504])
+session = requests.Session()
+session.mount("https://", HTTPAdapter(max_retries=retries))
+
+
+def check_response(response):
+    try:
+        response.raise_for_status()
+    except requests.HTTPError:
+        print(response.json())
+        raise
+
+
+def submit_id_mapping(from_db, to_db, taxid, ids):
+    request = requests.post(
+        f"{API_URL}/idmapping/run",
+        data={"from": from_db, "to": to_db, "taxId": taxid, "ids": ",".join(ids)},
+    )
+    check_response(request)
+    return request.json()["jobId"]
+
+
+def get_next_link(headers):
+    re_next_link = re.compile(r'<(.+)>; rel="next"')
+    if "Link" in headers:
+        match = re_next_link.match(headers["Link"])
+        if match:
+            return match.group(1)
+
+
+def check_id_mapping_results_ready(job_id):
+    while True:
+        request = session.get(f"{API_URL}/idmapping/status/{job_id}")
+        check_response(request)
+        j = request.json()
+        if "jobStatus" in j:
+            if j["jobStatus"] == "RUNNING":
+                print(f"Retrying in {POLLING_INTERVAL}s")
+                time.sleep(POLLING_INTERVAL)
+            else:
+                raise Exception(request["jobStatus"])
+        else:
+            return bool(j["results"] or j["failedIds"])
+
+
+def get_batch(batch_response, file_format, compressed):
+    batch_url = get_next_link(batch_response.headers)
+    while batch_url:
+        batch_response = session.get(batch_url)
+        batch_response.raise_for_status()
+        yield decode_results(batch_response, file_format, compressed)
+        batch_url = get_next_link(batch_response.headers)
+
+
+def combine_batches(all_results, batch_results, file_format):
+    if file_format == "json":
+        for key in ("results", "failedIds"):
+            if key in batch_results and batch_results[key]:
+                all_results[key] += batch_results[key]
+    elif file_format == "tsv":
+        return all_results + batch_results[1:]
+    else:
+        return all_results + batch_results
+    return all_results
+
+
+def get_id_mapping_results_link(job_id):
+    url = f"{API_URL}/idmapping/details/{job_id}"
+    request = session.get(url)
+    check_response(request)
+    return request.json()["redirectURL"]
+
+
+def decode_results(response, file_format, compressed):
+    if compressed:
+        decompressed = zlib.decompress(response.content, 16 + zlib.MAX_WBITS)
+        if file_format == "json":
+            j = json.loads(decompressed.decode("utf-8"))
+            return j
+        elif file_format == "tsv":
+            return [line for line in decompressed.decode("utf-8").split("\n") if line]
+        elif file_format == "xlsx":
+            return [decompressed]
+        elif file_format == "xml":
+            return [decompressed.decode("utf-8")]
+        else:
+            return decompressed.decode("utf-8")
+    elif file_format == "json":
+        return response.json()
+    elif file_format == "tsv":
+        return [line for line in response.text.split("\n") if line]
+    elif file_format == "xlsx":
+        return [response.content]
+    elif file_format == "xml":
+        return [response.text]
+    return response.text
+
+
+def get_xml_namespace(element):
+    m = re.match(r"\{(.*)\}", element.tag)
+    return m.groups()[0] if m else ""
+
+
+def merge_xml_results(xml_results):
+    merged_root = ElementTree.fromstring(xml_results[0])
+    for result in xml_results[1:]:
+        root = ElementTree.fromstring(result)
+        for child in root.findall("{http://uniprot.org/uniprot}entry"):
+            merged_root.insert(-1, child)
+    ElementTree.register_namespace("", get_xml_namespace(merged_root[0]))
+    return ElementTree.tostring(merged_root, encoding="utf-8", xml_declaration=True)
+
+
+def print_progress_batches(batch_index, size, total):
+    n_fetched = min((batch_index + 1) * size, total)
+    print(f"Fetched: {n_fetched} / {total}")
+
+def get_id_mapping_results_stream(url):
+    if "/stream/" not in url:
+        url = url.replace("/results/", "/results/stream/")
+    request = session.get(url)
+    check_response(request)
+    parsed = urlparse(url)
+    query = parse_qs(parsed.query)
+    file_format = query["format"][0] if "format" in query else "json"
+    compressed = (
+        query["compressed"][0].lower() == "true" if "compressed" in query else False
+    )
+    return decode_results(request, file_format, compressed)
+
+def mergeDictionary(dict_):
+    dict_3 = defaultdict(list)
+    for d in dict_: # you can list as many input dicts as you want here
+        for key, value in d.items():
+            dict_3[key].append(value)
+            # list(itertools.chain(*list_of_lists))
+    return dict_3
+
+@retry(delay=6)
+def get_pdb_structure_id(gene_list):
+    # Gene to Uniprot
+    job_id = submit_id_mapping(
+        from_db="Gene_Name", to_db="UniProtKB", taxid=9606, ids=gene_list
+    )
+
+    link = get_id_mapping_results_link(job_id)
+    mapping_result = get_id_mapping_results_stream(link + tsv_loader)
+    mapping_result = list(map(lambda x : x.split("\t"), mapping_result))
+    mapping_result_df = pd.DataFrame(mapping_result[1:], columns=mapping_result[0])
+    m_v = mapping_result_df[mapping_result_df['Reviewed'] == "reviewed"]
+
+    # Uniprot to PDB
+    m_v['pdb'] = m_v.apply(lambda x : Query(x['Entry']).search(), axis=1)
+    # m_v['pdbIDCount'] = m_v.apply(lambda x : 0 if x['pdb'] is None else int(len(x['pdb'])), axis=1)
+    m_v['pdbID'] = m_v.apply(lambda x : None if x['pdb'] is None else ';'.join(x['pdb']), axis=1)
+    m_v = m_v[['From', 'Entry', 'Reviewed', 'pdbID']]
+    m_v.columns = ['GENE_NAME', 'UniprotKB', 'Reviewed', 'pdbID']
+    
+    return m_v
 
 if __name__ == "__main__": 
     print("not main")
